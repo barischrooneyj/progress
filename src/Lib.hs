80080b0e708efdf19bf793049a3879b1163be087
Jeremy Barisch Rooney <barischrooneyj@gmail.com>
2018-05-24 20:53:14 +0200

    Move to example to State monad

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,13 +7,16 @@ module Lib where
 
 -- * Modeling the progress and targets of regions.
 
--- ** TODO: Suggest metrics for sibling regions.
--- ** TODO: Show progress of a region in terms of children.
--- ** TODO: Show progress of other regions on same metric.
+-- ** TODO: Suggest metrics for region via sibling regions.
+-- ** TODO: Get progress of a region in terms of children.
+-- ** TODO: Get progress of other regions on same metric.
 -- ** TODO: Better date input format.
--- ** TODO: Verify Eq and Ord instances.
+-- ** TODO: Correct Eq and Ord instances.
 
 import           Control.Monad.State
+import           Data.Map                          (Map)
+import qualified Data.Map                          as Map
+import           Data.Maybe                        (fromJust)
 import           Data.Set                          (Set)
 import qualified Data.Set                          as Set
 import           Numeric.Units.Dimensional         as Dim
@@ -23,67 +26,56 @@ import           Text.Pretty.Simple                (pPrint)
 -- ** Example usage.
 
 runExample :: IO ()
-runExample = runDBState $ do
+runExample = pPrint $ runDBState example
 
+example = do
   -- | Creating two users.
   gabriel <- addUser $ user "gabmass" "!@£$%^&*()"
-  jeremy <- addUser $ user "barischrooneyj" "!@£$%^&*()"
+  jeremy  <- addUser $ user "barischrooneyj" "!@£$%^&*()"
 
+  -- | A few regions including the root "World".
+  world   <- addRegion $ region jeremy "World"
+  germany <- addRegion $ region' jeremy "Germany" world
+  france  <- addRegion $ region' gabriel "France" world
+
+  -- | Some metrics, not connected to regions yet.
+  co2        <- addMetric $ metric gabriel "CO2 Emissions" (SI.liter Dim./ SI.second)
+  plasticTax <- addMetric $ metric' gabriel "Plastic Tax" "%"
+
+  -- | Some progress for the regions.
+  co2Germany        <- addProgress $ progress co2 germany [(47, 0), (40, 10)]
+  co2France         <- addProgress $ progress co2 france [(45, 0), (42, 10)]
+  plasticTaxFrance  <- addProgress $ progress plasticTax france [(10, 0), (10, 10)]
+  plasticTaxGermany <- addProgress $ progress plasticTax germany [(8, 0), (10, 10)]
+
+  -- | CO2 targets for France.
+  franceCo2Targets <- addTargets $ targets co2 france [
+    targetDecrease 35 "UN 2020" "1/1/2020",
+    targetDecrease 30 "National Plan" "1/1/2025"]
 
   pure ()
-
--- | Creating two users.
-gabriel = user "gabmass" "!@£$%^&*()"
-jeremy = user "barischrooneyj" "!@£$%^&*()"
-
-world = region jeremy "World" -- ^ I own the world.
-germany = region' jeremy "Germany" world  -- ^ And Germany, child of world.
-france = region' gabriel "France" world  -- ^ You own France, child of world.
-
--- | Two metrics.
-co2 = metric gabriel "CO2 Emissions" (SI.liter Dim./ SI.second)
-plasticTax = metric' gabriel "Plastic Tax" "%"
-
--- | Progress in those metrics for France and Germany.
-co2Germany = progress co2 germany [(47, 0), (40, 10)]
-co2France = progress co2 france [(45, 0), (42, 10)]
-plasticTaxFrance = progress plasticTax france [(10, 0), (10, 10)]
-plasticTaxGermany = progress plasticTax germany [(8, 0), (10, 10)]
-
--- | CO2 targets for France.
-franceCo2Targets = targets co2 france [
-  targetDecrease 35 "UN 2020" "1/1/2020",
-  targetDecrease 30 "National Plan" "1/1/2025"]
-
-someFunc :: IO ()
-someFunc = do
-  -- | Showing how we can print/parse a data type.
-  let franceString = show france
-      france'      = read franceString :: Region
-  pPrint france'
-  pPrint co2France
-  pPrint franceCo2Targets
 
 -- ** The data types.
 
 type Dimension     = Dimension'
 type DimensionName = String
 type Email         = String
+type ID            = Integer
 type Measurement   = (MetricValue, Seconds)
-type MetricId      = Int
+type MetricId      = ID
 type MetricName    = String
 type MetricSymbol  = String
 type MetricValue   = Double
 type PasswordHash  = String
 type Phone         = String
-type ProgressId    = Int
-type RegionId      = Int
+type ProgressId    = ID
+type RegionId      = ID
 type RegionName    = String
-type RepId         = Int
+type RepId         = ID
 type RepName       = String
 type Seconds       = Integer
 type TargetDesc    = String
-type TargetsId     = Int
+type TargetsId     = ID
 type Username      = String
 
 deriving instance Read Dimension'
@@ -101,28 +93,28 @@ data Metric = Metric {
   , _owner :: Username
   , _name  :: MetricName
   , _dim   :: Either Lib.Dimension MetricSymbol
-  } deriving (Read, Show)
+  } deriving (Eq, Ord, Read, Show)
 
 -- | Progress and targets for multiple metrics, under one region.
 data Region = Region {
-    _rgid     :: RegionId
-  , _rgowner  :: Username
-  , _name     :: RegionName
-  , _progress :: Set ProgressId
-  , _targets  :: Set TargetsId
-  , _reps     :: [RepId] -- ^ Order of importance.
-  , _parents  :: Set RegionId
-  , _children :: Set RegionId
-  } deriving (Read, Show)
+    _rgid      :: RegionId
+  , _rgowner   :: Username
+  , _name      :: RegionName
+  , _progresss :: Set ProgressId
+  , _targetss  :: Set TargetsId
+  , _reps      :: [RepId] -- ^ Order of importance.
+  , _parents   :: Set RegionId
+  , _children  :: Set RegionId
+  } deriving (Eq, Ord, Read, Show)
 
 -- | Measurements for one (metric, region).
 data Progress = Progress {
-    _pid    :: ProgressId
-  , _owner  :: Username
-  , _metric :: MetricId
-  , _region :: RegionId
-  , _reps   :: [RepId]
-  , _values :: [Measurement]
+    _pid     :: ProgressId
+  , _owner   :: Username
+  , _metric  :: MetricId
+  , _pregion :: RegionId
+  , _reps    :: [RepId]
+  , _values  :: [Measurement]
   } deriving (Eq, Ord, Read, Show)
 
 -- | Targets for one (metric, region).
@@ -130,7 +122,7 @@ data Targets = Targets {
     _tsid    :: TargetsId
   , _owner   :: Username
   , _metric  :: MetricId
-  , _region  :: RegionId
+  , _tregion :: RegionId
   , _targets :: [Target]
   } deriving (Eq, Ord, Read, Show)
 
@@ -187,7 +179,7 @@ targetIncrease m = Target $ NumTarget m True
 targetDecrease m = Target $ NumTarget m False
 targetBool     b = Target $ BoolTarget b
 
--- ** Helper functions.
+-- ** Helper functions on the data types.
 
 -- | A readable representation of dimensions.
 dimensionName :: Lib.Dimension -> DimensionName
@@ -204,17 +196,31 @@ dimensionName (Dim' l m t _i _th _n _j) =
 
 -- | Each field is like a table in a database.
 data DB = DB {
-    _users     :: Set User
-  , _metrics   :: Set Metric
-  , _regions   :: Set Region
-  , _progresss :: Set Progress
-  , _targetss  :: Set Targets
-  , _reps      :: Set Rep
+    _nextId      :: ID
+  , _users       :: Set User
+  , _metrics     :: Set Metric
+  , _regions     :: Map RegionId Region
+  , _dbProgresss :: Map ProgressId Progress
+  , _dbTargetss  :: Map TargetsId Targets
+  , _reps        :: Set Rep
   } deriving Show
 
 -- | A database with no data.
 emptyDB :: DB
-emptyDB = DB Set.empty Set.empty Set.empty Set.empty Set.empty Set.empty
+emptyDB = DB 0 Set.empty Set.empty Map.empty Map.empty Map.empty Set.empty
+
+-- | The next ID from the database.
+nextId :: State DB ID
+nextId = do
+  db <- get
+  put $ db { _nextId = _nextId db Prelude.+ 1 }
+  pure $ _nextId db
+
+-- | Run the given operations and print the resulting database.
+runDBState :: State DB a -> DB
+runDBState operations = evalState (operations >> get) emptyDB
+
+-- ** Adding to the database.
 
 -- | Add a user to the database.
 addUser :: User -> State DB User
@@ -223,6 +229,48 @@ addUser newUser = do
   put $ db { _users = Set.insert newUser (_users  db) }
   pure newUser
 
--- | Run the given operations and print the resulting database.
-runDBState :: State DB a -> IO ()
-runDBState operations = pPrint $ evalState (operations >> get) emptyDB
+-- | Add a region to the database.
+addRegion :: Region -> State DB Region
+addRegion newRegion = do
+  regionId <- nextId
+  db <- get
+  let newRegion' = newRegion { _rgid = regionId }
+  put $ db { _regions = Map.insert regionId newRegion' (_regions  db) }
+  pure newRegion'
+
+-- | Add a metric to the database.
+addMetric :: Metric -> State DB Metric
+addMetric newMetric = do
+  metricId <- nextId
+  db <- get
+  let newMetric' = newMetric { _mid = metricId }
+  put $ db { _metrics = Set.insert newMetric' (_metrics db) }
+  pure newMetric'
+
+-- | Add a progress to the database.
+addProgress :: Progress -> State DB Progress
+addProgress newProgress = do
+  progressId <- nextId
+  db <- get
+  let newProgress' = newProgress { _pid = progressId }
+      region_      = fromJust $ Map.lookup (_pregion newProgress) $ _regions db
+      newRegion    = region_ { _progresss = Set.insert progressId (_progresss region_) }
+  put $ db {
+      _dbProgresss = Map.insert progressId newProgress' (_dbProgresss db)
+    , _regions   = Map.insert (_pregion newProgress) newRegion $ _regions db
+    }
+  pure newProgress'
+
+-- | Add a targets to the database.
+addTargets :: Targets -> State DB Targets
+addTargets newTargets = do
+  targetsId <- nextId
+  db <- get
+  let newTargets' = newTargets { _tsid = targetsId }
+      region_     = fromJust $ Map.lookup (_tregion newTargets) $ _regions db
+      newRegion   = region_ { _targetss = Set.insert targetsId (_targetss region_) }
+  put $ db {
+      _dbTargetss = Map.insert targetsId newTargets' (_dbTargetss db)
+    , _regions   = Map.insert (_tregion newTargets) newRegion $ _regions db
+    }
+  pure newTargets'
